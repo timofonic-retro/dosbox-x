1f268fcd42d86aef07d5ec4a4b07cb61dcc8cbcb
qbix79 <qbix79@ed541006-0bf7-43e9-8c4d-6fc63c346d47>
2013-11-05 22:12:59 +0000

    Reenable the setting of the %CONFIG% variable when doing a CONFIG -get command (reported in forum)
    git-svn-id: http://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk@3842 ed541006-0bf7-43e9-8c4d-6fc63c346d47

--- src/misc/programs.cpp
+++ src/misc/programs.cpp
@@ -783,6 +783,7 @@ void CONFIG::Run(void) {
 					// it's a property name
 					std::string val = sec->GetPropValue(pvars[0].c_str());
 					WriteOut("%s",val.c_str());
+					first_shell->SetEnv("CONFIG",val.c_str());
 				}
 				break;
 			}
@@ -800,6 +801,7 @@ void CONFIG::Run(void) {
 					return;
 				}
 				WriteOut("%s",val.c_str());
+				first_shell->SetEnv("CONFIG",val.c_str());
 				break;
 			}
 			default:
