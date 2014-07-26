HellHibou/dosbox-plugin

enable full execution privileges for the dynamic core cache (windows DEP); raise generated data per block overflow buffer a bit

src/cpu/core_dynrec.cpp
@@ -27,6 +27,11 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+#if defined (_MSC_VER)
+#include <windows.h>
+#include <winbase.h>
+#endif
+
 #if (C_HAVE_MPROTECT)
 #include <sys/mman.h>
 

src/cpu/core_dynrec/cache.h
@@ -562,7 +562,14 @@ static void cache_init(bool enable) {
 		}
 		if (cache_code_start_ptr==NULL) {
 			// allocate the code cache memory
+#if defined (_MSC_VER)
+			cache_code_start_ptr=(Bit8u*)VirtualAlloc(0,CACHE_TOTAL+CACHE_MAXSIZE+PAGESIZE_TEMP-1+PAGESIZE_TEMP,
+				MEM_COMMIT,PAGE_EXECUTE_READWRITE);
+			if (!cache_code_start_ptr)
+				cache_code_start_ptr=(Bit8u*)malloc(CACHE_TOTAL+CACHE_MAXSIZE+PAGESIZE_TEMP-1+PAGESIZE_TEMP);
+#else
 			cache_code_start_ptr=(Bit8u*)malloc(CACHE_TOTAL+CACHE_MAXSIZE+PAGESIZE_TEMP-1+PAGESIZE_TEMP);
+#endif
 			if(!cache_code_start_ptr) E_Exit("Allocating dynamic cache failed");
 
 			// align the cache at a page boundary
@@ -624,6 +631,8 @@ static void cache_close(void) {
 		cache_blocks = NULL;
 	}
 	if (cache_code_start_ptr != NULL) {
+		### care: under windows VirtualFree() has to be used if
+		###       VirtualAlloc was used for memory allocation
 		free(cache_code_start_ptr);
 		cache_code_start_ptr = NULL;
 	}

timofonic-retro/dosbox-x

git-svn-id: http://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk@2879 ed541006-0bf7-43e9-8c4d-6fc63c346d47
Conflicts:
	src/cpu/core_dyn_x86.cpp
	src/cpu/core_dyn_x86/cache.h

src/cpu/core_dynrec.cpp
@@ -27,6 +27,11 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+#if defined (WIN32)
+#include <windows.h>
+#include <winbase.h>
+#endif
+
 #if (C_HAVE_MPROTECT)
 #include <sys/mman.h>
 

src/cpu/core_dynrec/cache.h
@@ -562,7 +562,14 @@ static void cache_init(bool enable) {
 		}
 		if (cache_code_start_ptr==NULL) {
 			// allocate the code cache memory
+#if defined (WIN32)
+			cache_code_start_ptr=(Bit8u*)VirtualAlloc(0,CACHE_TOTAL+CACHE_MAXSIZE+PAGESIZE_TEMP-1+PAGESIZE_TEMP,
+				MEM_COMMIT,PAGE_EXECUTE_READWRITE);
+			if (!cache_code_start_ptr)
+				cache_code_start_ptr=(Bit8u*)malloc(CACHE_TOTAL+CACHE_MAXSIZE+PAGESIZE_TEMP-1+PAGESIZE_TEMP);
+#else
 			cache_code_start_ptr=(Bit8u*)malloc(CACHE_TOTAL+CACHE_MAXSIZE+PAGESIZE_TEMP-1+PAGESIZE_TEMP);
+#endif
 			if(!cache_code_start_ptr) E_Exit("Allocating dynamic cache failed");
 
 			// align the cache at a page boundary
@@ -624,6 +631,8 @@ static void cache_close(void) {
 		cache_blocks = NULL;
 	}
 	if (cache_code_start_ptr != NULL) {
+		### care: under windows VirtualFree() has to be used if
+		###       VirtualAlloc was used for memory allocation
 		free(cache_code_start_ptr);
 		cache_code_start_ptr = NULL;
 	}